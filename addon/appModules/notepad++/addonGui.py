f7beffd6c5d8f9d7440da7171c62fede6c91e998
Tuukka Ojala <tuukka.ojala@gmail.com>
2016-07-01 23:24:19 +0300

    Implement a settings dialog for the add-on.

--- addon/appModules/notepad++/addonGui.py
+++ addon/appModules/notepad++/addonGui.py
@@ -1,5 +1,10 @@
 import wx
+
+import addonHandler
+import config
 import gui
+
+addonHandler.initTranslation()
 
 """File for managing GUI for the appModule for notepad++"""
 
@@ -7,8 +12,7 @@ class GuiManager(object):
 
 	def __init__(self):
 		def _popupMenu(evt):
-			#do popup work here, referencing the item if need be, like showing the dialog. Hint: Inherit  from gui.SettingsDialog
-			pass
+			gui.mainFrame._popupSettingsDialog(SettingsDialog)
 		self.prefsMenuItem  = item = gui.mainFrame.sysTrayIcon.preferencesMenu.Append(wx.ID_ANY, _("Notepad++..."))
 		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, _popupMenu, item)
 
@@ -23,3 +27,31 @@ class GuiManager(object):
 			gui.mainFrame.sysTrayIcon.preferencesMenu.RemoveItem(self.prefsMenuItem)
 		except wx.PyDeadObjectError:
 			pass
+
+class SettingsDialog(gui.SettingsDialog):
+	# Translators: Title for the settings dialog
+	title = _("Notepad++ settings")
+
+	def __init__(self, *args, **kwargs):
+		super(SettingsDialog, self).__init__(*args, **kwargs)
+
+	def makeSettings(self, settingsSizer):
+		# Translators: A setting for enabling/disabling line length indicator.
+		self.lineLengthIndicatorCheckBox = wx.CheckBox(self, wx.NewId(), label=_("Enable &line length indicator"))
+		self.lineLengthIndicatorCheckBox.SetValue(config.conf["notepadPp"]["lineLengthIndicator"])
+		settingsSizer.Add(self.lineLengthIndicatorCheckBox, border=10, flag=wx.BOTTOM)
+		maxLineLengthSizer = wx.BoxSizer(wx.HORIZONTAL)
+		# Translators: Setting for maximum line length used by line length indicator
+		maxLineLengthLabel = wx.StaticText(self, -1, label=_("&Maximum line length:"))
+		self.maxLineLengthEdit = wx.TextCtrl(self, wx.NewId())
+		self.maxLineLengthEdit.SetValue(str(config.conf["notepadPp"]["maxLineLength"]))
+		maxLineLengthSizer.AddMany([maxLineLengthLabel, self.maxLineLengthEdit])
+		settingsSizer.Add(maxLineLengthSizer, border=10, flag=wx.BOTTOM)
+
+	def postInit(self):
+		self.lineLengthIndicatorCheckBox.SetFocus()
+
+	def onOk(self, evt):
+		config.conf["notepadPp"]["lineLengthIndicator"] = self.lineLengthIndicatorCheckBox.IsChecked()
+		config.conf["notepadPp"]["maxLineLength"] = int(self.maxLineLengthEdit.GetValue())
+		super(SettingsDialog, self).onOk(evt)
