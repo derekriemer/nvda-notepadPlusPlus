2aff8cc36365d32a1bced04ff1971c826a059def
derek riemer <derek.riemer@colorado.edu>
2023-06-10 00:52:22 -0600

    2023.06.1 addon manifest update

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	"addon_description" : _("""Notepad++ App Module.
 This addon improves the accessibility of Notepad ++. To learn more, press the add-on help button."""),
 	# version
-	"addon_version" : "2022.05.1",
+	"addon_version" : "2023.06.1",
 	# Author(s)
 	"addon_author" : u"Derek Riemer <driemer.riemer@gmail.com>, Tuukka Ojala <tuukka.ojala@gmail.com>",
 	# URL for the add-on documentation support
@@ -30,7 +30,7 @@ This addon improves the accessibility of Notepad ++. To learn more, press the ad
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion" : "2021.2",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2022.1",
+	"addon_lastTestedNVDAVersion" : "2023.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
